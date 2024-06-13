d2a5ef2bb9a62b329d608c82f7943bf041ac6b85
Roman <7RomanGo@gmail.com>
2024-06-12 20:46:02 -0500

    stashing message

--- src/main/java/Boom.java
+++ src/main/java/Boom.java
@@ -2,5 +2,6 @@ public class Boom {
     public static void main(String[] args) {
         System.out.println("Hello brothers");
         System.out.println("Giy add after add");
+        System.out.println("Message from stash");
     }
 }
